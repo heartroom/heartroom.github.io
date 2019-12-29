a3e9857e9d932bb57e247d1242e28b92e1f44fc4
long <long275@126.com>
2019-12-29 03:23:05 +0000

    update

--- _site/assets/js/common.js
+++ _site/assets/js/common.js
@@ -1,9 +1,9 @@
 $(function(){ 
 	$.getJSON("/assets/data/quote.json", function (data) {
-		console.log(data);
+		//console.log(data);
 		if (data && data.length) {
 			var index = Math.round(Math.random() * (data.length - 1));
-			console.log(data[index]["Content"]);
+			//console.log(data[index]["Content"]);
 			$("#sub").html(data[index]["Content"] + "by &nbsp;&nbsp;" + data[index]["Author"]);
 		}
     });
