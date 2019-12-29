b873b648ac0b258c29b5f4e73fec22ffc9d415a4
long <long275@126.com>
2019-12-29 09:15:36 +0000

    update

--- assets/js/common.js
+++ assets/js/common.js
@@ -3,8 +3,11 @@ $(function(){
 		//console.log(data);
 		if (data && data.length) {
 			var index = Math.round(Math.random() * (data.length - 1));
-			//console.log(data[index]["Content"]);
-			$("#sub").html(data[index]["Content"] + "by &nbsp;&nbsp;" + data[index]["Author"]);
+			var content = data[index]["Content"];
+			if (data[index]["Author"] && data[index]["Author"] != '佚名') {
+				content += "&nbsp;&nbsp;--&nbsp;" + data[index]["Author"];
+			}
+			$("#sub").html(content);
 		}
     });
 }); 
